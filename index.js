94abe2a7fedb6a3b0b45cd6b542c66c35c6dd89c
Cainã <caina@webgoal.com.br>
2019-06-28 15:57:49 -0300

    Post  to create a project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projects=[];
+
+server.post("/projects", (req, res) => {
+  const { id, title, tasks } = req.body;
+
+  projects.push({ id, title, tasks });
+  return res.json(projects);
+});
+
+server.listen(6666);
